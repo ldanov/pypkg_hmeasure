afd6ee2b1d78116e3f7967538c915b4fc5581f77
ldanov <ldanov@users.noreply.github.com>
2019-06-08 13:26:19 +0200

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(name='hmeasure',
       author='Lyubomir Danov',
       # author_email='-',
       url='https://github.com/ldanov/pypkg_hmeasure',
-      packages=['hmeasure']
+      packages=['hmeasure'],
+      #TODO: find lower required versions
+      install_requires = ['numpy == 1.16.3', 'scikit_learn == 0.21.2', 'scipy == 1.2.1']
      )
