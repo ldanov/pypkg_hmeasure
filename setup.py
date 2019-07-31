e13cd21834d67465b407ac24b267c1c7980c723b
ldanov <ldanov@users.noreply.github.com>
2019-07-31 08:29:06 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
-#!/usr/bin/env python3
-
+import os
+import re
 from setuptools import setup
 from hmeasure._version import __version__
+
+def _parse_requirements(filepath):
+    with open(filepath, mode='r') as fp:
+        rqr_file = fp.readlines()
+
+    rqr_cl = [x for x in rqr_file
+              if not (re.match(r'^\s*$', x) or re.match(r'^#', x))]
+    rqr = [re.sub('==', '~=', x.replace('\n', '')) for x in rqr_cl]
+
+    return rqr
 
 __PROJECT_NAME__ = 'hmeasure'
 __DESCRIPTION__ = 'H-Measure Classification Metric'
 __AUTHORS__ = 'Lyubomir Danov'
 __URL__ = 'https://github.com/ldanov/pypkg_hmeasure'
 __PACKAGES__ = [__PROJECT_NAME__]
+req_loc = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+__INSTALL_REQUIRES__ = _parse_requirements(req_loc)
 
 setup(name=__PROJECT_NAME__,
       version=__version__,
       description=__DESCRIPTION__,
       author=__AUTHORS__,
-      # author_email='-',
       url=__URL__,
       packages=__PACKAGES__,
-      #TODO: find lower required versions
-      install_requires = ['numpy>=1.16.3', 'scikit_learn>=0.21.2', 'scipy>=1.2.1']
+      install_requires = __INSTALL_REQUIRES__
      )
