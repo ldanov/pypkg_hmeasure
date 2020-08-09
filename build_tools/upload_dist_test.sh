a5c01881109332c8a45cc19297c5169698985550
ldanov <18732847+ldanov@users.noreply.github.com>
2020-08-09 19:46:31 +0200

    more dev tools

--- build_tools/upload_dist_test.sh
+++ build_tools/upload_dist_test.sh
@@ -1,2 +1,4 @@
-# Replace dist/* with dist/*-[version_number]*
-# python3 -m twine upload --repository testpypi dist/*
+# In case of old versions pypi will reject upload
+# of files with same name
+# If this is the case replace dist/* with dist/*-[version_number]*
+python3 -m twine upload --repository testpypi dist/*
